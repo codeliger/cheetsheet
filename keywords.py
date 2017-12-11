e11b3246cc882bc6a3a3fc2745d6f580c0c6109b
Ben Tiessen <bentiessen@gmail.com>
2017-12-11 17:26:23 -0600

    Working with only 3 ununiquely paired questions

--- keywords.py
+++ keywords.py
@@ -34,20 +34,44 @@ def count_all_words(questions):
                 words[word] = 1
     return words
 
-def count_common_pair_words(questions):
-    paired_words = list()
+# create a list with every possible word pair for every commonly used word
+def list_common_pair_words(questions):
+    paired_words = dict()
     for question in questions:
         if len(question.unique_words) == 0:
             for fwi,first_word in enumerate(question.common_words):
                 for swi,second_word in enumerate(question.common_words):
-                    if first_word == second_word:
+                    if fwi == swi: # skip if indexes match
                         continue
-                    if length > 0:
-                        if (first_word,second_word) in paired_words:
-                            paired_words.remove((first_word,second_word))
+                    if (first_word,second_word) in paired_words:
+                        paired_words[(first_word,second_word)] +=1
                     else:
-                        paired_words.append((first_word,second_word))
+                        paired_words[(first_word,second_word)] = 1
+    print(len(paired_words),'paired words.')
+    # print(sorted(paired_words.values(),reverse=True))
+    return paired_words
+    
+# remove all common pairs that repeat because more than one question must have them
+def filter_common_pair_words(common_pairs):
+    return [common_pair for common_pair in common_pairs if common_pairs[common_pair] == 1]
 
+def assign_describing_words_to_non_unique_questions(questions,unique_pairs):
+    for unique_pair in unique_pairs:
+        for question in questions:
+            if len(question.unique_words) == 0:
+                if unique_pair[0] in question.words and unique_pair[1] in question.words:
+                    question.describing_words.append(unique_pair)
+
+def assign_unique_describing_word_to_questions(questions):
+    for question in questions:
+        if len(question.unique_words) > 0:
+            question.describing_words.append(question.unique_words[0])
+
+def count_questions_without_describing_words(questions):
+    print("=== Questions without describing words:")
+    for question in questions:
+        if len(question.describing_words) == 0:
+            print(question.question)
 
 def filter_unique_words(words):
     return [word for word in words if words[word] == 1]
@@ -84,9 +108,11 @@ def main():
     set_question_words(questions,unique_words,common_words)
     common,unique,word_count = count_all_question_words(questions,words)
     print(common,unique,common+unique,word_count)
-
-
-
+    common_pairs = list_common_pair_words(questions)
+    unique_pairs = filter_common_pair_words(common_pairs)
+    assign_describing_words_to_non_unique_questions(questions,unique_pairs)
+    assign_unique_describing_word_to_questions(questions)
+    count_questions_without_describing_words(questions)
 
 if __name__ == '__main__':
     main()
